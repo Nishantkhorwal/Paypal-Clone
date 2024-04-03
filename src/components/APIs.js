3d3a96ad5dd75047cae081eb70c2d1846822498e
NishantKhorwal <khorwalnishant@gmail.com>
2024-04-03 14:00:19 +0530

    removing anchor tags

--- src/components/APIs.js
+++ src/components/APIs.js
@@ -10,46 +10,46 @@ function APIs() {
         <div className='flex flex-col md:flex-row-reverse md:w-4/5  md:me-10 md:ms-28 md:mb-20'>
         <div className='flex flex-col w-3/4 md:w-[630px] ms-12 rounded-xl shadow-lg pb-8 mb-10 md:mb-0'>
             <div className='bg-green-400 py-10 mb-5 flex items-center justify-center rounded-t-xl'>
-                <img src='work3.png' className='w-[7rem] h-[5em] '></img>
+                <img src='work3.png' className='w-[7rem] h-[5em] ' alt=''></img>
                 <h4 className='relative left-[-72px] text-lg text-white'>API</h4>
 
 
             </div>
             <h1 className='text-xl  ps-4 mb-4'>APIs</h1>
             <p className='text-sm  ps-4 pe-11 mb-4'>PayPal offers APIs for new and legacy integrations.</p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>Rest APIs</a></p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>NVP/SOAP APIs</a></p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>GraphQL APIs</a></p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>Rest APIs</p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>NVP/SOAP APIs</p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>GraphQL APIs</p>
 
 
         </div>
         <div className='flex flex-col w-3/4 ms-12 rounded-xl shadow-lg pb-8 mb-10 md:mb-0 md:w-[630px]'>
             <div className='bg-green-400 py-10 mb-5 flex items-center justify-center rounded-t-xl'>
-                <img src='work3.png' className='w-[7rem] h-[5em] '></img>
+                <img src='work3.png' className='w-[7rem] h-[5em] ' alt=''></img>
                 <h4 className='relative left-[-72px] text-lg text-white'>API</h4>
 
 
             </div>
             <h1 className='text-xl  ps-4 mb-4'>APIs</h1>
             <p className='text-sm  ps-4 pe-11 mb-4'>PayPal offers APIs for new and legacy integrations.</p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>Rest APIs</a></p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>NVP/SOAP APIs</a></p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>GraphQL APIs</a></p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>Rest APIs</p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>NVP/SOAP APIs</p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>GraphQL APIs</p>
 
 
         </div>
         <div className='flex flex-col w-3/4 ms-12 rounded-xl shadow-lg pb-8 md:w-[630px]'>
             <div className='bg-green-400 py-10 mb-5 flex items-center justify-center rounded-t-xl'>
-                <img src='work3.png' className='w-[7rem] h-[5em] '></img>
+                <img src='work3.png' className='w-[7rem] h-[5em] ' alt=''></img>
                 <h4 className='relative left-[-72px] text-lg text-white'>API</h4>
 
 
             </div>
             <h1 className='text-xl  ps-4 mb-4'>APIs</h1>
             <p className='text-sm  ps-4 pe-11 mb-4'>PayPal offers APIs for new and legacy integrations.</p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>Rest APIs</a></p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>NVP/SOAP APIs</a></p>
-            <p><a className=' ps-4 font-semibold text-blue-700 mb-3'>GraphQL APIs</a></p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>Rest APIs</p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>NVP/SOAP APIs</p>
+            <p className=' ps-4 font-semibold text-blue-700 mb-3'>GraphQL APIs</p>
 
 
         </div>
@@ -58,7 +58,7 @@ function APIs() {
 
         <div className=' mt-10 md:mt-0 ms-10 w-4/5 flex justify-start bg-gray-200 items-center rounded-full mb-5 md:w-4/5 md:ms-36 md:py-5'>
             <div className='ms-5 me-4'><FontAwesomeIcon className='text-lg ' icon={faCodeCompare} /></div>
-            <p>To browse PayPal SDKs, see our GitHub library. <a className='cursor-pointer text-blue-600'>Learn More</a></p>
+            <p>To browse PayPal SDKs, see our GitHub library. <p className='cursor-pointer text-blue-600'>Learn More</p></p>
 
         </div>
 
